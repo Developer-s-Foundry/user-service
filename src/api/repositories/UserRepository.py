e8a5334801e1a70870baff64636ec1643467281d
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2025-02-28 11:38:25 +0100

    refactor UserRepository methods to static methods

--- src/api/repositories/UserRepository.py
+++ src/api/repositories/UserRepository.py
@@ -2,27 +2,33 @@ from ..models.postgres.User import User
 
 
 class UserRepository:
-    async def add(self, user: User) -> User:
+    @staticmethod
+    async def add(user: User) -> User:
         await user.asave()
         return user
 
-    async def find_by_id(self, id: str) -> User:
+    @staticmethod
+    async def find_by_id(id: str) -> User:
         return await User.objects.filter(id=id).afirst()
 
-    async def find_by_email(self, email: str) -> User:
+    @staticmethod
+    async def find_by_email(email: str) -> User:
         return await User.objects.filter(email=email).afirst()
 
-    async def list(self, filter: dict = {}) -> list[User]:
+    @staticmethod
+    async def list(filter: dict = {}) -> list[User]:
         return await list(User.objects.filter(**filter))
 
-    async def update_by_user(self, user: User, updates: dict | None = None) -> User:
+    @staticmethod
+    async def update_by_user(user: User, updates: dict | None = None) -> User:
         if updates:
             for key, value in updates.items():
                 setattr(user, key, value)
             await user.asave()
         return user
 
-    async def update_by_id(self, id: str, updates: dict | None = None) -> User:
+    @staticmethod
+    async def update_by_id(id: str, updates: dict | None = None) -> User:
         user: User | None = User.objects.filter(id=id).afirst()
         if user and updates:
             for key, value in updates.items():
