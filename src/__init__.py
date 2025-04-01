61a1fb9e52e51a11cdb5070ab768199fe5302a66
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2025-04-01 21:49:19 +0100

    Bump version to 1.2.0

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 __name__ = "df-wallet-user-service"
 __display_name__ = "DF Wallet User Service API"
 __description__ = "Developers Foundry Wallet System User Service RESTful API"
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 __author__ = "Ogidi Ifechukwu (ogidiifechukwu6@gmail.com)"
