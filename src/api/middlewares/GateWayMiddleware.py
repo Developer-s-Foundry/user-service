58e49d2edaab7f0ee78043a6df8356e252c1df2e
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2025-04-16 14:44:45 +0100

    fixed pyright lint errors

--- src/api/middlewares/GateWayMiddleware.py
+++ src/api/middlewares/GateWayMiddleware.py
@@ -1,11 +1,9 @@
 import hmac
-import base64
 import hashlib
-from http import HTTPStatus
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django.http import HttpRequest
-from ninja.errors import HttpError
+from ninja.errors import AuthenticationError
 from ninja.security import APIKeyHeader
 from ninja.openapi.schema import OpenAPISchema
 
@@ -18,15 +16,16 @@ class GateWayAuth(APIKeyHeader):
         self.logger = logger
         super().__init__()
 
-    def authenticate(self, request: HttpRequest, key: str) -> str | None:
-        api_key = request.headers.get("X-API-GATEWAY-KEY")
-        api_timestamp = request.headers.get("X-API-GATEWAY-TIMESTAMP")
-        api_signature = request.headers.get("X-API-GATEWAY-SIGNATURE")
-        user_id = request.headers.get("X-USER-ID")
-        user_email = request.headers.get("X-USER-EMAIL")
+    def authenticate(self, request: HttpRequest, key: str | None ) -> str | None:
 
-        if not all([api_key, api_timestamp, api_signature, user_id, user_email]):
-            message = "Missing required headers!"
+        try:
+            api_key = request.headers["X-API-GATEWAY-KEY"]
+            api_timestamp = request.headers["X-API-GATEWAY-TIMESTAMP"]
+            api_signature = request.headers["X-API-GATEWAY-SIGNATURE"]
+            user_id = request.headers["X-USER-ID"]
+            user_email = request.headers["X-USER-EMAIL"]
+        except KeyError as e:
+            message = f"Missing required header: {e}"
             self.logger.error(
                 {
                     "activity_type": "Authenticate User",
@@ -34,7 +33,7 @@ class GateWayAuth(APIKeyHeader):
                     "metadata": {"headers": request.headers},
                 }
             )
-            raise HttpError(HTTPStatus.UNAUTHORIZED, message)
+            raise AuthenticationError(message=message)
 
         valid_api_key = api_gateway["key"]
         if api_key != valid_api_key:
@@ -46,9 +45,9 @@ class GateWayAuth(APIKeyHeader):
                     "metadata": {"headers": request.headers},
                 }
             )
-            raise HttpError(HTTPStatus.UNAUTHORIZED, message)
+            raise AuthenticationError(message=message)
 
-        self._verify_signature(valid_api_key, valid_api_key, api_timestamp)
+        self._verify_signature(valid_api_key, api_signature, api_timestamp)
 
         self.logger.debug(
             {
@@ -80,13 +79,12 @@ class GateWayAuth(APIKeyHeader):
                     "metadata": {"signature": signature},
                 }
             )
-            raise HttpError(HTTPStatus.UNAUTHORIZED, message)
+            raise AuthenticationError(message=message)
 
-        timestamp = int(timestamp)
-        current_time = datetime.now().timestamp() * 1000
-        lifespan = api_gateway["expires_at"] * 60 * 60 * 1000
-
-        if abs(current_time - timestamp) > lifespan:
+        initial_time = datetime.fromtimestamp(int(timestamp) / 1000)
+        valid_window = initial_time + timedelta(minutes=api_gateway["expires_at"])
+        if valid_window < datetime.now():
+        
             message = "Signature expired!"
             self.logger.error(
                 {
@@ -95,15 +93,15 @@ class GateWayAuth(APIKeyHeader):
                     "metadata": {"timestamp": timestamp},
                 }
             )
-            raise HttpError(HTTPStatus.UNAUTHORIZED, message)
+            raise AuthenticationError(message=message)
 
         return True
 
     def generate_signature(self, api_key: str, timestamp: str) -> str:
         signature = hmac.new(
             key=api_key.encode(), msg=timestamp.encode(), digestmod=hashlib.sha256
-        ).digest()
-        return base64.b64encode(signature).decode("utf-8")
+        ).hexdigest()
+        return signature
 
 
 def get_authentication() -> GateWayAuth:
